56b221785a17d66353bc6fdb45391f6e6ae53ffc
Ihor <u96ok69@gmail.com>
2025-09-16 22:54:54 +0300

    make vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App.jsx";
 import "./index.css";
+import "modern-normalize";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
